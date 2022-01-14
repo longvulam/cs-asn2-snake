f8e3d7f448b338d3165ed62199dbd81f903c2fb4
Lam Long Vu <longvulam91@gmail.com>
2022-01-13 22:16:23 -0800

    Add GameNetwork project & modules.

---
+++ GameNetwork/MessageReceiver.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameNetwork
+{
+    public class MessageReceiver
+    {
+
+        // Define a delegate named LogHandler, which will encapsulate
+        // any method that takes a string as the parameter and returns no value
+        public delegate void MessageReceivedHandler(string message);
+
+        // Define an Event based on the above Delegate
+        public event MessageReceivedHandler OnReceivedEvent;
+
+        private IPAddress mcastAddress;
+        private int mcastPort;
+        private Socket mcastSocket;
+        private MulticastOption mcastOption;
+        private bool isListening = false;
+
+        public bool IsListening { get => isListening; set => isListening = value; }
+
+        public MessageReceiver(string ipAddress, int port)
+        {
+            bool isValidAddr = IPAddress.TryParse(ipAddress, out mcastAddress);
+            if (isValidAddr)
+            {
+                mcastPort = port;
+            }
+            else
+            {
+                throw new ArgumentException("Could not parse IP address", ipAddress);
+            }
+        }
+
+        public void StartListen()
+        {
+            try
+            {
+                mcastSocket = new Socket(AddressFamily.InterNetwork,
+                               SocketType.Dgram,
+                               ProtocolType.Udp);
+
+
+                IPAddress localIP = IPAddress.Any;
+                EndPoint localEP = new IPEndPoint(localIP, mcastPort);
+                mcastSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+
+                mcastSocket.Bind(localEP);
+
+
+                // Define a MulticastOption object specifying the multicast group 
+                // address and the local IPAddress.
+                // The multicast group address is the same as the address used by the server.
+                mcastOption = new MulticastOption(mcastAddress, localIP);
+
+                mcastSocket.SetSocketOption(SocketOptionLevel.IP,
+                                            SocketOptionName.AddMembership,
+                                            mcastOption);
+
+                byte[] bytes = new byte[100];
+                int receivedBytes = 0;
+                EndPoint groupEP = new IPEndPoint(mcastAddress, mcastPort);
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                isListening = true;
+                while (isListening)
+                {
+                    receivedBytes = mcastSocket.ReceiveFrom(bytes, ref groupEP);
+
+                    string message = Encoding.ASCII.GetString(bytes, 0, receivedBytes);
+
+                    OnMessageReceived(message);
+                }
+
+                mcastSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.ToString());
+                throw;
+            }
+        }
+
+        public void StopListen()
+        {
+            isListening = false;
+        }
+
+        private void OnMessageReceived(string message)
+        {
+            if (OnReceivedEvent != null)
+            {
+                OnReceivedEvent.Invoke(message);
+            }
+        }
+    }
+}
---
+++ GameNetwork/MessageSender.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GameNetwork
+{
+    public class MessageSender
+    {
+        private IPAddress mcastAddress;
+        private int mcastPort;
+        private Socket mcastSocket;
+        private IPEndPoint endPoint;
+
+        public MessageSender(string ipAddress, int port)
+        {
+            bool isValidAddr = IPAddress.TryParse(ipAddress, out mcastAddress);
+            if (isValidAddr)
+            {
+                mcastPort = port;
+                endPoint = new IPEndPoint(mcastAddress, mcastPort);
+            }
+            else
+            {
+                throw new ArgumentException("Could not parse IP address", ipAddress);
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            try
+            {
+                //Send multicast packets to the listener.
+                mcastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                byte[] asBytes = Encoding.ASCII.GetBytes(message);
+                mcastSocket.SendTo(asBytes, asBytes.Length, SocketFlags.None, endPoint);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.ToString());
+            }
+
+            mcastSocket.Close();
+        }
+    }
+}
