c03e7238689fb3c77786ab63255e23f0573ce317
Lam Long Vu <longvulam91@gmail.com>
2022-01-14 22:22:37 -0800

    Add server to GameNetwork.

--- GameNetwork/Program.cs
+++ GameNetwork/Program.cs
@@ -8,27 +8,10 @@ namespace GameNetwork
         private const string IpAddress = "230.0.0.1";
         private const int Port = 11000;
 
-        static void handleMessage (string message)
-        {
-            Console.WriteLine(message);
-        }
-
         static void Main(string[] args)
         {
-            MessageReceiver receiver = new MessageReceiver(IpAddress, Port);
-            receiver.OnReceivedEvent += handleMessage;
 
-            Thread thread = new Thread(new ThreadStart(receiver.StartListen));
-            thread.Start();
-
-            while(!receiver.IsListening) { };
-
-            MessageSender sender = new MessageSender(IpAddress, Port);
-            string message = Console.ReadLine();
-            sender.SendMessage(message);
-
-            receiver.StopListen();
-            Console.ReadKey();
+            new Server(IpAddress, Port).Start();
         }
     }
 }
---
+++ GameNetwork/Server.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameNetwork
+{
+    internal class Server
+    {
+        private MessageReceiver receiver;
+        private MessageSender sender;
+
+        private Server() { }
+
+        public Server(string ipAddress, int port)
+        {
+            sender = new MessageSender(ipAddress, port);
+            receiver = new MessageReceiver(ipAddress, port);
+        }
+
+        public void Start()
+        {
+            receiver.OnReceivedEvent += OnReceived;
+            Thread listenThread = new Thread(new ThreadStart(receiver.StartListen));
+            listenThread.Start();
+
+            Thread senderThread = new Thread(new ThreadStart(OnSend));
+            senderThread.Start();
+        }
+
+        private async void OnSend()
+        {
+
+            int IntervalTime = 1000;
+            while (true)
+            {
+                await Task.Delay(IntervalTime);
+                BroadcastState();
+            }
+        }
+
+        private void BroadcastState()
+        {
+            try
+            {
+                // ServerState.getState() -> broadcast to game clients
+                var messsage = new BroadcastMessage
+                {
+                    dest = BroadcastMessageDestination.Server,
+                    body = ""
+                };
+                string json = JsonConvert.SerializeObject(messsage);
+                sender.SendMessage(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void OnReceived(string otherPlayerJson)
+        {
+            try
+            {
+                var message = JsonConvert.DeserializeObject<BroadcastMessage>(otherPlayerJson);
+
+                var isToServer = message.dest == BroadcastMessageDestination.Server;
+                if (isToServer == false) return;
+
+                Console.WriteLine($"Received: {otherPlayerJson}");
+
+                // ServerLogic.UpdatePlayerState(otherPlayerJson);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
